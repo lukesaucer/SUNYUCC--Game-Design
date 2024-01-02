edadf94176ba6551f78a04bf6acbd5175ddb951a
Lucas Saucer <saucerL05@my.sunyulster.edu>
2024-01-02 18:19:34 -0500

    Commit MissionDemolition and ApplePicker
    Commit MissionDemolition and ApplePicker assignments.

---
+++ Game Design 1/ApplePicker/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+	[Header("Inscribed")]
+	//prefab for instantiating apples
+	public GameObject applePrefab;
+	
+	// Speed at which the AppleTree moves
+	public float speed = 1f;
+	
+	// Distance where AppleTree turns around
+	public float leftAndRightEdge = 10f;
+	
+	// Chance that the AppleTree will change directions
+	public float changeDirChance = 0.1f;
+	
+	// Seconds between Apple Instantiation
+	public float appleDropDelay = 1f;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Start dropping apples
+		Invoke("DropApple", 2f);
+    }
+	
+	void DropApple(){
+		GameObject apple = Instantiate<GameObject>(applePrefab);
+		apple.transform.position = transform.position;
+		Invoke("DropApple", appleDropDelay);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic Movement
+		Vector3 pos = transform.position; // gets tree position
+		pos.x += speed * Time.deltaTime; // calculates new position
+		transform.position = pos;        // move the tree
+		
+		if (pos.x < -leftAndRightEdge){
+			speed = Mathf.Abs(speed);  // Move right
+		} else if (pos.x > leftAndRightEdge){
+			speed = -Mathf.Abs(speed); // Move leftAndRightEdge
+		} //else if (Random.value < changeDirChance){
+		//	speed *= -1;
+		//}
+    }
+	
+	void FixedUpdate(){
+		if (Random.value < changeDirChance){
+			speed *= -1;
+		}
+	}
+}
---
+++ Game Design 1/MissionDemolition/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour
+{
+[Header("Set in Inspector")]
+public GameObject prefabProjectile;
+public float velocityMult = 8f;
+
+[Header("Set Dynamically")]
+public GameObject launchPoint;
+public Vector3 launchPos;
+public GameObject projectile;
+public bool aimingMode;
+private Rigidbody projectileRigidbody;
+
+void Awake(){
+	Transform launchPointTrans = transform.Find("LaunchPoint");
+	launchPoint = launchPointTrans.gameObject;
+	launchPoint.SetActive(false);
+	launchPos = launchPointTrans.position;
+}
+
+void OnMouseEnter(){
+//	print("Slingshot:OnMouseEnter()");
+launchPoint.SetActive(true);
+}
+
+void OnMouseExit(){
+//	print("Slingshot:OnMouseExit()");
+launchPoint.SetActive(false);
+}
+
+void OnMouseDown(){
+	aimingMode = true;
+	projectile = Instantiate(prefabProjectile) as GameObject;
+	projectile.transform.position = launchPos;
+	projectile.GetComponent<Rigidbody>().isKinematic = true;
+	projectileRigidbody = projectile.GetComponent<Rigidbody>();
+	projectileRigidbody.isKinematic = true;
+}
+
+void Update(){
+	if (!aimingMode) return;
+	
+	Vector3 mousePos2D = Input.mousePosition; // c
+	mousePos2D.z = -Camera.main.transform.position.z;
+	Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );
+
+	// Find the delta from the launchPos to the mousePos3D
+	Vector3 mouseDelta = mousePos3D-launchPos;
+
+	// Limit mouseDelta to the radius of the Slingshot SphereCollider // d
+	float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+	if (mouseDelta.magnitude > maxMagnitude) {
+		mouseDelta.Normalize();
+		mouseDelta *= maxMagnitude;
+		}
+	
+	// Move the projectile to this new position
+	Vector3 projPos = launchPos + mouseDelta;
+	projectile.transform.position = projPos;
+	if ( Input.GetMouseButtonUp(0) ) { // e
+	
+		// The mouse has been released
+		aimingMode = false;
+		projectileRigidbody.isKinematic = false;
+		projectileRigidbody.velocity = -mouseDelta * velocityMult;
+		FollowCam.POI = projectile;
+		projectile = null;
+		}
+	}
+}
